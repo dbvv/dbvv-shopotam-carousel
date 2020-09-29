75c521bfc71f2517f363f3d04779d87431ffec75
Daniil Borovkov <daniilborowkow@gmail.com>
2020-09-29 07:18:28 +0600

    Initial commit

---
+++ dbvv-shopotam-carousel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Dbvv Shopotam Carousel
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     dbvv-shopotam-carousel
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Dbvv_Shopotam_Carousel
+ */
+
+// Your code starts here.
