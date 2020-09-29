831902b683e646937f0b8e1152d3eef465c397b8
Daniil Borovkov <daniilborowkow@gmail.com>
2020-09-29 17:20:36 +0600

    Added product parser

---
+++ inc/parser.php
@@ -0,0 +1,30 @@
+<?php
+
+use KubAT\PhpSimple\HtmlDomParser;
+
+class Parser {
+	private $url;
+	private $parsedData;
+
+	public function __construct($url) {
+		$this->url = $url;
+	}
+
+	public function parse() {
+		$str = file_get_contents($this->url);
+		//echo $str;
+		$dom = HtmlDomParser::str_get_html( $str );
+		$meta = $dom->find('[type="application/ld+json"]');
+
+		if (count($meta) == 0) {
+			return false;
+		}
+
+		$this->parsedData = json_decode($meta[0]->innertext, true);
+
+	}
+
+	public function getParsedData() {
+		return $this->parsedData;
+	}
+}
---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+require "vendor/autoload.php";
+require 'inc/parser.php';
+
+$urls = [
+	"https://shopotam.ru/market/detyam/tovary-dlya-shkoly/elektronika-dlya-ucheby/smartfony/apple/iphone-xs-max-6-5-oled-2688x1242-a12-bionic-64gb-802-11ac-2x-12mp-7mp-face-id-ios-12-7110355.html",
+];
+
+foreach ($urls as $url) {
+	$parser = new Parser($url);
+	$parser->parse();
+	$data = $parser->getParsedData();
+	var_dump($data);
+}
